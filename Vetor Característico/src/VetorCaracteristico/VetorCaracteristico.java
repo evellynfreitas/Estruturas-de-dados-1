5128d6db36f28cd90201a3e8f947843aaf28c671
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-05-16 18:28:55 -0300

    Método de inserção

--- Vetor Característico/src/VetorCaracteristico/VetorCaracteristico.java
+++ Vetor Característico/src/VetorCaracteristico/VetorCaracteristico.java
@@ -9,4 +9,13 @@ public class VetorCaracteristico {
 		tamanho = n;
 		vetor = new int[tamanho];
 	}
+	
+	public boolean inserirElemento(int x) {
+		if(x > tamanho || x <= 0)
+			return false;
+		else {
+			vetor[x-1] = 1;
+			return true;
+		}
+	}
 }
