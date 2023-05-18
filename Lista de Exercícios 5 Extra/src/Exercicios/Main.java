8e50f64cf295a9646aed1f5cf58cfb401b0a5891
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-05-18 10:28:03 -0300

    Add files via upload

---
+++ Lista de Exercícios 5 Extra/src/Exercicios/Main.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Lista lista = new Lista();
+		TesteListaSingular teste = new TesteListaSingular();
+		
+		teste.valida(lista);
+	}
+}
