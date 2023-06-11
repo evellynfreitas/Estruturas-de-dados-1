b9c59feeafdc896db5cd2cfad9fa8f1b704a119e
Evellyn Freitas <80793635+evellynfreitas@users.noreply.github.com>
2023-06-11 18:57:06 -0300

    Exercício 6

---
+++ Lista de Exercícios 6/Exercicio6/Conj.java
@@ -0,0 +1,244 @@
+package Exercicio6;
+public class Conj
+{
+	private Elo prim;  /* Referência para primeiro elemento. */
+
+	/* Classe auxiliar para guardar cada elemento do conjunto. */
+	private class Elo
+	{                 
+		int dado;
+		Elo prox;
+		/*
+		public Elo()
+		{ 
+			prox = null;
+		}
+		 */
+		public Elo(int elem)
+		{
+			dado = elem;
+			prox = null;
+		}
+		/*
+		public Elo(int elem, Elo proxElem)
+		{ 
+			dado = elem;
+			prox = proxElem;
+		}*/
+	}
+
+	/* Construtor que cria um conjunto vazio. */
+	public Conj()
+	{
+		prim = null;
+	}
+
+	/* Método privado para realizar uma cópia de um outro conjunto. */
+	private void copia(Conj conj2)
+	{
+		Elo ult = null, q;
+
+		prim = null;
+		
+		for (Elo p = conj2.prim; p != null; p = p.prox)
+		{
+			q = new Elo(p.dado);
+			
+			if (ult == null)
+				prim = q;
+			else
+				ult.prox = q;
+			
+			ult = q;
+		}
+	}
+
+	/* Método privado para realizar uma cópia de um outro conjunto. */
+	public void apaga()
+	{
+		for (Elo p = prim; p != null; p = prim)
+		{
+			prim = prim.prox;
+			p.prox = null;
+		}
+	}
+
+	/* Simula uma sobrecarga do operador de atribuição. */
+	public Conj atribui(Conj conj2){
+		if (this != conj2)
+		{
+			apaga();
+			copia(conj2);
+		}
+		
+		return this;
+	}
+
+	/* Testa se o conjunto está vazio. */
+	public boolean vazio()
+	{
+		return prim == null;
+	}
+
+	/* Teste de pertinência. Usa fato de estar ordenado. */
+	public boolean pertence(int valor)
+	{
+		Elo p;
+		
+		for (p = prim; ((p != null) && (p.dado < valor)); p = p.prox);
+
+		if ((p == null) || (p.dado > valor)) return false;	  
+
+		return true;
+	}
+
+	/* Inserção de elemento no conjunto. Usa fato de estar ordenado.
+	   Retorna false se elemento já estava lá. */
+	public boolean insere(int valor)
+	{
+		Elo p = prim, ant = null;
+
+		for (p = prim; (p != null); p = p.prox)
+		{
+			if (p.dado == valor) return false;
+			if (p.dado > valor) break;
+			
+			ant = p;
+		}
+		
+		Elo q = new Elo(valor);
+		
+		if (p == prim) 
+			prim = q;
+		else
+			ant.prox = q;
+		
+		q.prox = p;
+		
+		return true;
+	}
+
+	/* Remoção de elemento do conjunto. Usa fato de estar ordenado.
+	   Retorna false se elemento não estava lá. */
+	public boolean remove(int valor)
+	{
+		Elo p = prim, ant = null;
+
+		for (p = prim; (p != null); p = p.prox)
+		{
+			if (p.dado > valor) return false;
+			if (p.dado == valor) break;	 
+			
+			ant = p;
+		}
+		
+		if (p == null)
+			return false;
+		
+		if (p == prim) 
+			prim = prim.prox;
+		else
+			ant.prox = p.prox;
+
+		p = null;
+		
+		return true;
+	}
+
+	/* Método para união de conjuntos. Une conjunto com cj2 e retorna novo conjunto com a união. 
+	 * Usa fato de conjuntos estarem ordenados e percorre as listas em paralelo. */
+	public Conj uniao(Conj conj2)
+	{
+		Elo q, p1 = prim, p2 = conj2.prim, ult = null;
+		Conj uniao = new Conj(); 
+		
+		while ( (p1 != null) || (p2 != null) )
+		{
+			if ( (p1 != null) && ( (p2 == null) || (p1.dado < p2.dado)))
+			{
+				q = new Elo(p1.dado);
+				p1 = p1.prox;
+			}
+			else
+			{
+				q = new Elo(p2.dado);
+				if ((p1 != null) && (p1.dado == p2.dado))
+					p1 = p1.prox;
+				p2 = p2.prox;
+			} 
+
+			if (ult == null) 
+				uniao.prim = q;
+			else
+				ult.prox = q;
+			
+			ult = q;
+		}
+		
+		return uniao;
+	}
+
+	/* Método para intersecao de conjuntos. Calcula intersecao do conjunto com cj2 e retorna novo conjunto com a intersecao. 
+	 * Usa fato de conjuntos estarem ordenados e percorre as listas em paralelo. */
+	public Conj intersecao(Conj conj2)
+	{
+		Elo q, p1 = prim, p2 = conj2.prim, ult = null;
+		Conj inter = new Conj();
+
+		while ( (p1 != null) && (p2 != null) )
+		{
+			if (p1.dado < p2.dado)
+			{
+				p1 = p1.prox;
+			}
+			else if(p2.dado < p1.dado)
+			{
+				p2 = p2.prox;
+			}
+			else
+			{
+				q = new Elo(p1.dado);
+				
+				p1 = p1.prox;
+				p2 = p2.prox;
+				
+				if (ult == null) 
+					inter.prim = q;
+				else
+					ult.prox = q;
+
+				ult = q;
+			}
+		}
+
+		return inter;
+	}
+
+	/* Retorna cardinalidade do conjunto */
+	public int tamanho()
+	{
+		int tam = 0;
+		Elo p;
+
+		for(p = prim; p != null; p = p.prox)
+			tam++;
+		
+		return tam;
+	}
+
+	/* Imprime todos os elementos do conjunto */
+	public void imprime()
+	{
+		Elo p;
+		
+		System.out.println("Elementos do conjunto");
+		
+		for(p = prim; p != null; p = p.prox)
+			System.out.print(p.dado + " ");
+		
+		System.out.println();
+	}
+
+	
+
+}
